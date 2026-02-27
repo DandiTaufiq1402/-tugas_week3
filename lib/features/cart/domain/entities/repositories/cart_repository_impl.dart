e6db3104b3a3e0dc11878e8dd5efb250b247d672
Dandi Taufiqurrahman <dandi.taufiq50@gmail.com>
2026-02-27 13:20:51 +0700

    Implementasi nyata dari kontrak repository, misalnya menyimpan data ke list lokal atau nantinya bisa diubah ke API.

---
+++ lib/features/cart/domain/entities/repositories/cart_repository_impl.dart
@@ -0,0 +1,19 @@
+import '../../domain/entities/product.dart';
+import '../../domain/repositories/cart_repository.dart';
+
+class CartRepositoryImpl implements CartRepository {
+  final List<Product> items = [];
+
+  @override
+  List<Product> getCartItems() => List.unmodifiable(items);
+
+  @override
+  void addItem(Product product) => items.add(product);
+
+  @override
+  void removeAllItems() => items.clear();
+
+  @override
+  bool isItemInCart(String productId) =>
+      items.any((p) => p.id == productId);
+}
