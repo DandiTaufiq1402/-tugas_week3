935ccf6229d8be2876231d1e7e89211ab854eddc
Dandi Taufiqurrahman <dandi.taufiq50@gmail.com>
2026-02-27 13:23:11 +0700

    Jembatan antara UI dan Data. Mengatur state aplikasi dan memanggil

---
+++ lib/features/cart/domain/entities/providers/cart_provider.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import '../../domain/entities/product.dart';
+import '../../domain/repositories/cart_repository.dart';
+
+class CartProvider extends ChangeNotifier {
+  final CartRepository repository;
+
+  // Dependency Injection melalui constructor
+  CartProvider({required this.repository});
+
+  List<Product> get items => repository.getCartItems();
+
+  void addItem(Product product) {
+    repository.addItem(product);
+    notifyListeners(); // Memberitahu UI untuk rebuild
+  }
+
+  void removeAll() {
+    repository.removeAllItems();
+    notifyListeners();
+  }
+
+  bool isInCart(String productId) =>
+      repository.isItemInCart(productId);
+}
