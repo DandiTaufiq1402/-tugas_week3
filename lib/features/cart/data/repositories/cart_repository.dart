038745577d1d811b62aa93a9fc6d4b6fae4c2fc1
Dandi Taufiqurrahman <dandi.taufiq50@gmail.com>
2026-03-02 10:34:19 +0700

    menambahkan lainnya

--- lib/features/cart/data/repositories/cart_repository.dart
+++
@@ -1,8 +0,0 @@
-import '../entities/product.dart';
-
-abstract class CartRepository {
-  List<Product> getCartItems();
-  void addItem(Product product);
-  void removeAllItems();
-  bool isItemInCart(String productId);
-}
